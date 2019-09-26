6697ef96754143f16a7d535e249fb6fdf9c67cf3
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-09-26 20:06:24 +0300

    evoltion plusieurs enfants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 void main(){
   runApp(new MyApp());
@@ -45,12 +46,72 @@ class _Home extends State<Home>{
       ),
       backgroundColor: Colors.white,
       body: new Container(
-        color: Colors.teal,
-        height:  MediaQuery.of(context).size.height / 2,
-        width: 4 * MediaQuery.of(context).size.width / 5,
-        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 12  ,
-          right: MediaQuery.of(context).size.width / 10, left: MediaQuery.of(context).size.width / 10
+        color: Colors.white,
+        // height:  MediaQuery.of(context).size.height / 2,
+        // width: 4 * MediaQuery.of(context).size.width / 5,
+        margin: EdgeInsets.all(10),
+
+        child: new Center(
+          child: new Card(
+            color: Colors.teal,
+            elevation: 15,
+            child: new Container(
+              color: Colors.white,
+              height: 350,
+              width: 300,
+              margin: EdgeInsets.only(top: 30),
+              padding: EdgeInsets.only(top: 20),
+              child:new Column(
+                children: <Widget>[
+                  new Row(
+                    children: <Widget>[
+                      Container(
+                        height: 40,
+                        width: 40,
+                        margin: EdgeInsets.only(left: 35),
+                        child: new Image.asset(
+                        'img/logo.png',
+                        // fit: BoxFit.cover,
+                        ),
+                      ),
+                      Text( " Se Connecter ",
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 25,
+                        fontWeight: FontWeight.bold )
+                      ), 
+
+                    ],
+                  ),
+                  Form(
+                    child: new Column(
+                      children: <Widget>[
+                        new Container(
+                            margin: EdgeInsets.only(top: 20, left: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                labelText: 'Entrer votre addresse mail',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                        ),
+                        
+                      ],
+                    ),
+                  )
+
+                 
+                  
+                  
+                ],
+
+              ) 
+            
+            ),
+          )
         ),
+          // right: MediaQuery.of(context).size.width / 10, left: MediaQuery.of(context).size.width / 10
+        
 
       ),
 
